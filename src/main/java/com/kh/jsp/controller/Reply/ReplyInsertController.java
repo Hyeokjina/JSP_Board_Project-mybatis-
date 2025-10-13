737ff06344e8090d49084a483a31430e59e24640
Hheokjina <lsh6439ll@gmail.com>
2025-10-13 10:26:48 +0900

    docs(board,reply controller) : update controller.sevlet form
    - 컨트롤러 서블릿 폼 통일해보기

--- src/main/java/com/kh/jsp/controller/Reply/ReplyInsertController.java
+++ src/main/java/com/kh/jsp/controller/Reply/ReplyInsertController.java
@@ -2,15 +2,39 @@ package com.kh.jsp.controller.Reply;
 
 import com.kh.jsp.model.vo.Reply;
 import com.kh.jsp.service.ReplyService;
-import jakarta.servlet.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
+/**
+ * Servlet implementation class ReplyInsertController
+ */
 @WebServlet("/insertReply.re")
 public class ReplyInsertController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
     private ReplyService service = new ReplyService();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReplyInsertController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    	 doPost(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String refBnoStr = request.getParameter("refBno");
         String replyWriterStr = request.getParameter("replyWriter");
@@ -37,4 +61,3 @@ public class ReplyInsertController extends HttpServlet {
         response.getWriter().print(result); // 1이면 성공, 0이면 실패
     }
 }
-
